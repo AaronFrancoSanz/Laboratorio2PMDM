0f0917d7752bfc3a41d6d3e9c7cd9c243a24fc89
AaronFrancoSanz <4076442@alu.murciaeduca.es>
2026-09-25 19:00:40 +0200

    Commit: Botón con imagen añadida, cambiados tamaños que no permitian pulsar botones

--- app/src/main/java/com/example/laboratorio2pmdm/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio2pmdm/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.laboratorio2pmdm
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.RadioButton
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
